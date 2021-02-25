429509ff4006457273d9aeb586e0f11e68396742
Daniel Andrade <eldani17@gmail.com>
2021-02-25 18:11:24 -0300

    implements promise and async and await

--- finger-exercises/ecmascript/02-promises/index.js
+++ finger-exercises/ecmascript/02-promises/index.js
@@ -1,9 +1,17 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
-export function delay() {
-
+export function delay(time) {
+  return createPromise(time);
 }
 
-export function asyncDelay() {
+export function asyncDelay(time) {
+    return createPromise(time);
+}
 
+function createPromise(time) {
+  return new Promise((resolver, reject) => {
+    resolver(time);
+    let error = new Error('This time is too much !');
+    reject(error);
+  });
 }
