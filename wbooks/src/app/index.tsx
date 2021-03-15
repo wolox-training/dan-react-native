3f5ce873496db8e1f1687778ec99fc8d5290c728
Daniel Andrade <eldani17@gmail.com>
2021-03-15 09:45:53 -0300

    added tabbar

--- wbooks/src/app/index.tsx
+++ wbooks/src/app/index.tsx
@@ -10,40 +10,86 @@
  */
 import 'react-native-gesture-handler';
 import React from 'react';
-import { Image, StyleSheet } from 'react-native';
+import { Image, StyleSheet, View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, StackNavigationOptions } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import BookList from '@screens/bookList';
 import BookHome from '@screens/bookHome';
-import { white } from '@constants/colors';
-import fondo from '@assets/bc_nav_bar.png';
+import { white, cerulean, dustyGray } from '@constants/colors';
+import headerImageBackground from '@assets/bc_nav_bar.png';
+import { bookHomeScreenRoute, bookListScreenRoute, dummyScreenRoute } from '@constants/routes';
+import Icon from 'react-native-vector-icons/FontAwesome';
+// import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const AppStack = createStackNavigator();
+import Dummy from './screens/dummy';
 
+const customHeaderNavigator: StackNavigationOptions = {
+  headerTintColor: cerulean,
+  headerTitleAlign: 'center',
+  headerTitleStyle: {
+    textTransform: 'uppercase'
+  }
+  // headerBackground: () => (
+  //   <Image
+  //     style={StyleSheet.absoluteFill}
+  //     source={headerImageBackground}
+  //     // width={450}
+  //     // height={60}
+  //     resizeMode="stretch"
+  //   />
+  // )
+};
+
+const BookListStack = createStackNavigator();
+const BookListStackScreen = () => {
+  return (
+    <BookListStack.Navigator initialRouteName={bookListScreenRoute} screenOptions={customHeaderNavigator}>
+      <BookListStack.Screen name={bookHomeScreenRoute} component={BookHome} options={{ title: 'Home' }} />
+      <BookListStack.Screen name={bookListScreenRoute} component={BookList} options={{ title: 'Library' }} />
+    </BookListStack.Navigator>
+  );
+};
+
+const DummyStack = createStackNavigator();
+const DammyStackScreen = () => {
+  return (
+    <DummyStack.Navigator initialRouteName={dummyScreenRoute} screenOptions={customHeaderNavigator}>
+      <DummyStack.Screen name={dummyScreenRoute} component={Dummy} options={{ title: 'Dummy' }} />
+    </DummyStack.Navigator>
+  );
+};
+
+const AppTab = createBottomTabNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <AppStack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerTintColor: white,
-          headerTitleAlign: 'center',
-          // headerTitleStyle: {
-          //   textTransform: 'uppercase'
-          // },
-          headerBackground: () => (
-            <Image
-              style={StyleSheet.absoluteFill}
-              source={fondo}
-              // width={450}
-              // height={60}
-              resizeMode="stretch"
-            />
-          )
+      <AppTab.Navigator
+        // screenOptions={({ route }) => ({
+        //   tabBarIcon: ({ focused, color, size }) => {
+        //     let iconName = '';
+
+        //     if (route.name === bookListScreenRoute) {
+        //       iconName = focused ? 'address-card' : 'ios-information-circle-outline';
+        //     } else if (route.name === dummyScreenRoute) {
+        //       iconName = focused ? 'android' : 'ios-list';
+        //     }
+
+        //     // You can return any component that you like here!
+        //     return <Icon name={iconName} size={size} color={color} />;
+        //   }
+        // })}
+        tabBarOptions={{
+          activeTintColor: cerulean,
+          inactiveTintColor: dustyGray
         }}>
-        <AppStack.Screen name="Home" component={BookHome} options={{ title: 'Inicio' }} />
-        <AppStack.Screen name="HomeList" component={BookList} options={{ title: 'Lista de Libros' }} />
-      </AppStack.Navigator>
+        <AppTab.Screen
+          name={bookListScreenRoute}
+          component={BookListStackScreen}
+          options={{ title: 'Library' }}
+        />
+        <AppTab.Screen name={dummyScreenRoute} component={DammyStackScreen} />
+      </AppTab.Navigator>
     </NavigationContainer>
   );
 };
---
+++ wbooks/src/constants/routes.ts
@@ -0,0 +1,3 @@
+export const bookListScreenRoute = 'BookList';
+export const bookHomeScreenRoute = 'BookHome';
+export const dummyScreenRoute = 'Dummy';
