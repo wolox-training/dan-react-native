503972fc8997fbe2c19d1f3c2b835cd6d5fbcfb8
Daniel Andrade <eldani17@gmail.com>
2021-03-17 11:52:58 -0300

    the customIconTabBar component is renamed to customTabBarIcon. styles are extracted to a styles.ts file. active icon files are removed (tintColor prop is used). the icon search is extracted to a utils.ts file

--- wbooks/src/app/assets/tabBar/index.ts
+++ wbooks/src/app/assets/tabBar/index.ts
@@ -1,25 +1,15 @@
 import icAddNew from './icAddNew.png';
-import icAddNewActive from './icAddNewActive.png';
 import icLibrary from './icLibrary.png';
-import icLibraryActive from './icLibraryActive.png';
 import icMyRentals from './icMyRentals.png';
-import icMyRentalsActive from './icMyRentalsActive.png';
 import icSettings from './icSettings.png';
-import icSettingsActive from './icSettingsActive.png';
 import icWishlist from './icWishlist.png';
-import icWishlistActive from './icWishlistActive.png';
 
 const iconTabBar = {
   icAddNew,
-  icAddNewActive,
   icLibrary,
-  icLibraryActive,
   icMyRentals,
-  icMyRentalsActive,
   icSettings,
-  icSettingsActive,
-  icWishlist,
-  icWishlistActive
+  icWishlist
 };
 
 export default iconTabBar;
---
+++ wbooks/src/app/components/customTabBarIcon/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+import { cerulean, dustyGray } from '@constants/colors';
+
+export default StyleSheet.create({
+  active: {
+    tintColor: cerulean
+  },
+  inactive: {
+    tintColor: dustyGray
+  }
+});
