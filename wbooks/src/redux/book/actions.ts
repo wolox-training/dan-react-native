dd1258299b55b37d600e722133701419820c0a0b
Daniel Andrade <eldani17@gmail.com>
2021-04-05 14:42:32 -0300

    The BooksResponse interface was created. A Promise is simulated in the BookService getBooks service

--- wbooks/src/redux/book/actions.ts
+++ wbooks/src/redux/book/actions.ts
@@ -1,6 +1,5 @@
 import { Dispatch } from 'redux';
-import { BOOKS_MOCK } from '@constants/mockBooks';
-// import BookService from '@services/bookService';
+import BookService from '@services/bookService';
 
 export const actions = {
   GET_BOOKS: '@@BOOKS/GET_BOOKS',
@@ -9,11 +8,9 @@ export const actions = {
 } as const;
 
 export const actionCreators = {
-  getBooks: () => (dispatch: Dispatch) => {
+  getBooks: () => async (dispatch: Dispatch) => {
     dispatch({ type: actions.GET_BOOKS });
-    const response = { ok: true, data: BOOKS_MOCK, problem: '' };
-    // const resp = await BookService.getBooks();
-    // console.log('resp', resp);
+    const response = await BookService.getBooks();
     if (response.ok) {
       dispatch({ type: actions.GET_BOOKS_SUCCESS, payload: response.data });
     } else {
--- wbooks/src/services/bookService.ts
+++ wbooks/src/services/bookService.ts
@@ -1,7 +1,16 @@
-import api from '@config/api';
+// import api from '@config/api';
+import { BOOKS_MOCK } from '@constants/mockBooks';
+import { BooksResponse } from '@interfaces/booksResponse';
+
+function getBooksMock() {
+  return new Promise<BooksResponse>(resolve => {
+    setTimeout(() => resolve({ ok: true, data: BOOKS_MOCK }), 1000);
+  });
+}
 
 const BookService = {
-  getBooks: () => api.get('/book')
+  // getBooks: () => api.get('/book')
+  getBooks: () => getBooksMock()
 };
 
 export default BookService;
